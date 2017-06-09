a0b2c704df68c41643f59737799a775b07f6368d
Marivaldo Malaquias <marivaldo@gmail.com>
2017-06-09 15:39:04 -0400

    first commit

---
+++ src/app/_providers/error-handler.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import swal from 'sweetalert2';
+import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+
+@Injectable()
+export class ErrorHandlerService {
+
+  constructor(private loading: SlimLoadingBarService) {}
+
+  handleError(error: any): void {
+    const message = error.message ? error.message : error.toString();
+    if (error.status === 0) {
+      swal({title: 'Erro', text: 'Sem comunicação com o servidor. ' + message, type: 'error'});
+    } else if (error.status === 422) {
+      const errors = error.json();
+
+      let _message = '';
+
+      for (const err of Object.keys(errors)) {
+        for (const msg of errors[err]) {
+          _message = _message + msg + '<br>';
+        }
+      }
+
+      swal({title: 'Erro', html: 'Não foi possível concluir a transação. <br>' + _message, type: 'error'});
+    } else {
+      swal({title: 'Erro', text: 'Não foi possível concluir a transação. ' + message, type: 'error'});
+    }
+    this.loading.complete();
+  }
+
+
+}
